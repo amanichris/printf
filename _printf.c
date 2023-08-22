760c1dba056662b044ebb978fc6af96db81cf8aa
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2023-08-22 13:20:59 -0700

    _printf

--- _printf.c
+++ _printf.c
@@ -17,7 +17,7 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return (-1);
 
-	va_start(list, format);
+	va_start(list_of_args, format);
 
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
